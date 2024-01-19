f0b07acb275467310133ca7d5c0bbbab9151408c
karanMalhii <karanmalhii1313@gmail.com>
2024-01-19 16:43:16 +0530

    my first demo commit

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Contact from "./pages/Contact.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "",
+    element: <Home />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </React.StrictMode>
+);
