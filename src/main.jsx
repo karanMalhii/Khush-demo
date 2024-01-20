893bb23ef70dc14e2a2115c9048d25235abd250b
karanmalhii <karanmalhii1313@gmail.com>
2024-01-20 21:54:40 +0530

    error fixed with bikram

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     element: <About />,
   },
   {
-    path: "",
+    path: "home",
     element: <Home />,
   },
   {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
-      Home
+      Home back to <Link to="/contact">contact</Link>
     </div>
-  )
+  );
 }
